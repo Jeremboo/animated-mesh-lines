06e7b5df7695cf56ccae4d11ac3d19b5f8889ccc
Jérémie Boulay <jeremi.boulay@gmail.com>
2018-12-19 12:29:25 -0500

    :fire: clear demos

--- app/demos/demo4/index.js
+++ app/demos/demo4/index.js
@@ -1,10 +1,8 @@
 import {
-  Color, Vector3, SphereBufferGeometry,
-  Mesh, Raycaster, MeshBasicMaterial,
+  Color,
 } from 'three';
 
 import Engine from 'utils/engine';
-import Stars from 'objects/Stars';
 import AnimatedText3D from 'objects/AnimatedText3D';
 import LineGenerator from 'objects/LineGenerator';
 
@@ -13,7 +11,6 @@ import getRandomItem from 'utils/getRandomItem';
 
 import CameraPositionHandlerWithMouse from 'decorators/CameraPositionHandlerWithMouse';
 import FullScreenInBackground from 'decorators/FullScreenInBackground';
-import PostProcessing from 'decorators/PostProcessing';
 
 import app from 'App';
 
@@ -27,40 +24,21 @@ import './style.styl';
  */
 
 @FullScreenInBackground
-@PostProcessing
 @CameraPositionHandlerWithMouse({ x: 1, y: 1 }, 0.1)
 class CustomEngine extends Engine {}
-
 const engine = new CustomEngine();
-engine.camera.position.z = 2;
-engine.addBloomEffect({
-  resolutionScale: 0.5,
-  kernelSize: 4,
-  distinction: 0.01,
-}, 1);
-
-
+engine.camera.position.z = 6;
 
 /**
  * * *******************
  * * TITLE
  * * *******************
  */
-const text = new AnimatedText3D('Boreal Sky', { color: '#FFFFFF', size: 0.1, wireframe: true, opacity: 1, });
-text.position.x -= text.basePosition * 0.5;
-engine.add(text);
-
-/**
- * * *******************
- * * STARS
- * * *******************
- */
-const stars = new Stars();
-stars.update = () => {
-  stars.rotation.y -= 0.0004;
-  stars.rotation.x -= 0.0002;
-};
-engine.add(stars);
+const text = new AnimatedText3D('Colors', { color: '#ffffff', size: 0.4, wireframe: false, opacity: 1, });
+text.position.x -= text.basePosition * 0.55;
+text.position.y = -0.9;
+text.position.z = 2;
+text.rotation.x = -0.1;
 
 /**
  * * *******************
@@ -68,78 +46,59 @@ engine.add(stars);
  * * *******************
  */
 
-const radius = 4;
-const origin = new Vector3();
-const direction = new Vector3();
-const raycaster = new Raycaster();
-const geometry = new SphereBufferGeometry(radius, 32, 32, 0, 3.2, 4, 2.1);
-const material = new MeshBasicMaterial({ wireframe: true, visible: false });
-const sphere = new Mesh(geometry, material);
-engine.add(sphere);
-sphere.position.z = 2;
+const RADIUS_START = 0.3;
+const RADIUS_START_MIN = 0.1;
+const Z_MIN = -1;
 
-const COLORS = ['#FFFAFF', '#0A2463', '#3E92CC', '#723bb7', '#efd28e', '#3f9d8c'].map((col) => new Color(col));
+const Z_INCREMENT = 0.08;
+const ANGLE_INCREMENT = 0.025;
+const RADIUS_INCREMENT = 0.02;
+
+const COLORS = ['#dc202e', '#f7ed99', '#2d338b', '#76306b', '#ea8c2d'].map((col) => new Color(col));
 const STATIC_PROPS = {
-  transformLineMethod: p => p,
+  transformLineMethod: p => p * 1.5,
 };
 
+const position = { x: 0, y: 0, z: 0 };
 class CustomLineGenerator extends LineGenerator {
   addLine() {
-    // V1 Regular and symetric lines ---------------------------------------------
-    // i += 0.1;
-    // let a = i;
-    // let y = 12;
-    // let incrementation = 0.1;
-    // V2 ---------------------------------------------
-    let incrementation = 0.1;
-    let y = getRandomFloat(-radius * 0.6, radius * 1.8);
-    let a = Math.PI * (-25) / 180;
-    let aMax = Math.PI * (200) / 180;
+    if (this.lines.length > 400) return;
+
+    let z = Z_MIN;
+    let radius = (Math.random() > 0.8) ? RADIUS_START_MIN : RADIUS_START;
+    let angle = getRandomFloat(0, Math.PI * 2);
 
     const points = [];
-    while (a < aMax) {
-      a += 0.2;
-      y -= incrementation;
-      origin.set(radius * Math.cos(a), y, radius * Math.sin(a));
-      direction.set(-origin.x, 0, -origin.z);
-      direction.normalize();
-      raycaster.set(origin, direction);
+    while (z < engine.camera.position.z) {
+      position.x = Math.cos(angle) * radius;
+      position.y = Math.sin(angle) * radius;
+      position.z = z;
 
-      // save the points
-      const intersect = raycaster.intersectObject(sphere, true);
-      if (intersect.length) {
-        points.push(intersect[0].point.x, intersect[0].point.y, intersect[0].point.z);
-      }
+      // incrementation
+      z += Z_INCREMENT;
+      angle += ANGLE_INCREMENT;
+      radius += RADIUS_INCREMENT;
+
+      // push
+      points.push(position.x, position.y, position.z);
     }
 
-    if (points.length === 0) return;
-
-    if (Math.random() > 0.5) {
-      // Low lines
-      super.addLine({
-        visibleLength: getRandomFloat(0.01, 0.2),
-        points,
-        speed: getRandomFloat(0.003, 0.008),
-        color: getRandomItem(COLORS),
-        width: getRandomFloat(0.01, 0.1),
-      });
-    } else {
-      // Fast lines
-      super.addLine({
-        visibleLength: getRandomFloat(0.05, 0.2),
-        points,
-        speed: getRandomFloat(0.01, 0.1),
-        color: COLORS[0],
-        width: getRandomFloat(0.01, 0.01),
-      });
-    }
+    // Low lines
+    super.addLine({
+      visibleLength: getRandomFloat(0.1, 0.4),
+      // visibleLength: 1,
+      points,
+      // speed: getRandomFloat(0.001, 0.002),
+      speed: getRandomFloat(0.001, 0.005),
+      color: getRandomItem(COLORS),
+      width: getRandomFloat(0.01, 0.06),
+    });
   }
 }
 const lineGenerator = new CustomLineGenerator({
-  frequency: 0.99,
+  frequency: 0.9,
 }, STATIC_PROPS);
 engine.add(lineGenerator);
-
 
 /**
  * * *******************
@@ -151,15 +110,16 @@ engine.start();
 const tlShow = new TimelineLite({ delay: 0.2, onStart: () => {
   lineGenerator.start();
 }});
-tlShow.to('.overlay', 2, { autoAlpha: 0 });
-tlShow.fromTo(engine.lookAt, 3, { y: -4 }, { y: 0, ease: Power3.easeOut }, '-=2');
-tlShow.add(text.show, '-=2');
+tlShow.to('.overlay', 5, { autoAlpha: 0 });
+tlShow.add(() => {
+  engine.add(text);
+  text.show();
+}, '-=2');
 
 // Hide
 app.onHide((onComplete) => {
   const tlHide = new TimelineLite();
-  tlHide.to(engine.lookAt, 2, { y: -6, ease: Power3.easeInOut });
+  tlHide.to('.overlay', 0.5, { autoAlpha: 1, onComplete }, 0.1);
   tlHide.add(text.hide, 0);
   tlHide.add(lineGenerator.stop);
-  tlHide.to('.overlay', 0.5, { autoAlpha: 1, onComplete }, '-=1.5');
 });
--- app/demos/demo5/index.js
+++ app/demos/demo5/index.js
@@ -1,8 +1,10 @@
 import {
-  Color,
+  Color, Vector3, SphereBufferGeometry,
+  Mesh, Raycaster, MeshBasicMaterial,
 } from 'three';
 
 import Engine from 'utils/engine';
+import Stars from 'objects/Stars';
 import AnimatedText3D from 'objects/AnimatedText3D';
 import LineGenerator from 'objects/LineGenerator';
 
@@ -11,6 +13,7 @@ import getRandomItem from 'utils/getRandomItem';
 
 import CameraPositionHandlerWithMouse from 'decorators/CameraPositionHandlerWithMouse';
 import FullScreenInBackground from 'decorators/FullScreenInBackground';
+import PostProcessing from 'decorators/PostProcessing';
 
 import app from 'App';
 
@@ -24,21 +27,40 @@ import './style.styl';
  */
 
 @FullScreenInBackground
+@PostProcessing
 @CameraPositionHandlerWithMouse({ x: 1, y: 1 }, 0.1)
 class CustomEngine extends Engine {}
+
 const engine = new CustomEngine();
-engine.camera.position.z = 6;
+engine.camera.position.z = 2;
+engine.addBloomEffect({
+  resolutionScale: 0.5,
+  kernelSize: 4,
+  distinction: 0.01,
+}, 1);
+
+
 
 /**
  * * *******************
  * * TITLE
  * * *******************
  */
-const text = new AnimatedText3D('Colors', { color: '#ffffff', size: 0.4, wireframe: false, opacity: 1, });
-text.position.x -= text.basePosition * 0.55;
-text.position.y = -0.9;
-text.position.z = 2;
-text.rotation.x = -0.1;
+const text = new AnimatedText3D('Boreal Sky', { color: '#FFFFFF', size: 0.1, wireframe: true, opacity: 1, });
+text.position.x -= text.basePosition * 0.5;
+engine.add(text);
+
+/**
+ * * *******************
+ * * STARS
+ * * *******************
+ */
+const stars = new Stars();
+stars.update = () => {
+  stars.rotation.y -= 0.0004;
+  stars.rotation.x -= 0.0002;
+};
+engine.add(stars);
 
 /**
  * * *******************
@@ -46,59 +68,78 @@ text.rotation.x = -0.1;
  * * *******************
  */
 
-const RADIUS_START = 0.3;
-const RADIUS_START_MIN = 0.1;
-const Z_MIN = -1;
+const radius = 4;
+const origin = new Vector3();
+const direction = new Vector3();
+const raycaster = new Raycaster();
+const geometry = new SphereBufferGeometry(radius, 32, 32, 0, 3.2, 4, 2.1);
+const material = new MeshBasicMaterial({ wireframe: true, visible: false });
+const sphere = new Mesh(geometry, material);
+engine.add(sphere);
+sphere.position.z = 2;
 
-const Z_INCREMENT = 0.08;
-const ANGLE_INCREMENT = 0.025;
-const RADIUS_INCREMENT = 0.02;
-
-const COLORS = ['#dc202e', '#f7ed99', '#2d338b', '#76306b', '#ea8c2d'].map((col) => new Color(col));
+const COLORS = ['#FFFAFF', '#0A2463', '#3E92CC', '#723bb7', '#efd28e', '#3f9d8c'].map((col) => new Color(col));
 const STATIC_PROPS = {
-  transformLineMethod: p => p * 1.5,
+  transformLineMethod: p => p,
 };
 
-const position = { x: 0, y: 0, z: 0 };
 class CustomLineGenerator extends LineGenerator {
   addLine() {
-    if (this.lines.length > 400) return;
-
-    let z = Z_MIN;
-    let radius = (Math.random() > 0.8) ? RADIUS_START_MIN : RADIUS_START;
-    let angle = getRandomFloat(0, Math.PI * 2);
+    // V1 Regular and symetric lines ---------------------------------------------
+    // i += 0.1;
+    // let a = i;
+    // let y = 12;
+    // let incrementation = 0.1;
+    // V2 ---------------------------------------------
+    let incrementation = 0.1;
+    let y = getRandomFloat(-radius * 0.6, radius * 1.8);
+    let a = Math.PI * (-25) / 180;
+    let aMax = Math.PI * (200) / 180;
 
     const points = [];
-    while (z < engine.camera.position.z) {
-      position.x = Math.cos(angle) * radius;
-      position.y = Math.sin(angle) * radius;
-      position.z = z;
+    while (a < aMax) {
+      a += 0.2;
+      y -= incrementation;
+      origin.set(radius * Math.cos(a), y, radius * Math.sin(a));
+      direction.set(-origin.x, 0, -origin.z);
+      direction.normalize();
+      raycaster.set(origin, direction);
 
-      // incrementation
-      z += Z_INCREMENT;
-      angle += ANGLE_INCREMENT;
-      radius += RADIUS_INCREMENT;
-
-      // push
-      points.push(position.x, position.y, position.z);
+      // save the points
+      const intersect = raycaster.intersectObject(sphere, true);
+      if (intersect.length) {
+        points.push(intersect[0].point.x, intersect[0].point.y, intersect[0].point.z);
+      }
     }
 
-    // Low lines
-    super.addLine({
-      visibleLength: getRandomFloat(0.1, 0.4),
-      // visibleLength: 1,
-      points,
-      // speed: getRandomFloat(0.001, 0.002),
-      speed: getRandomFloat(0.001, 0.005),
-      color: getRandomItem(COLORS),
-      width: getRandomFloat(0.01, 0.06),
-    });
+    if (points.length === 0) return;
+
+    if (Math.random() > 0.5) {
+      // Low lines
+      super.addLine({
+        visibleLength: getRandomFloat(0.01, 0.2),
+        points,
+        speed: getRandomFloat(0.003, 0.008),
+        color: getRandomItem(COLORS),
+        width: getRandomFloat(0.01, 0.1),
+      });
+    } else {
+      // Fast lines
+      super.addLine({
+        visibleLength: getRandomFloat(0.05, 0.2),
+        points,
+        speed: getRandomFloat(0.01, 0.1),
+        color: COLORS[0],
+        width: getRandomFloat(0.01, 0.01),
+      });
+    }
   }
 }
 const lineGenerator = new CustomLineGenerator({
-  frequency: 0.9,
+  frequency: 0.99,
 }, STATIC_PROPS);
 engine.add(lineGenerator);
+
 
 /**
  * * *******************
@@ -110,16 +151,15 @@ engine.start();
 const tlShow = new TimelineLite({ delay: 0.2, onStart: () => {
   lineGenerator.start();
 }});
-tlShow.to('.overlay', 5, { autoAlpha: 0 });
-tlShow.add(() => {
-  engine.add(text);
-  text.show();
-}, '-=2');
+tlShow.to('.overlay', 2, { autoAlpha: 0 });
+tlShow.fromTo(engine.lookAt, 3, { y: -4 }, { y: 0, ease: Power3.easeOut }, '-=2');
+tlShow.add(text.show, '-=2');
 
 // Hide
 app.onHide((onComplete) => {
   const tlHide = new TimelineLite();
-  tlHide.to('.overlay', 0.5, { autoAlpha: 1, onComplete }, 0.1);
+  tlHide.to(engine.lookAt, 2, { y: -6, ease: Power3.easeInOut });
   tlHide.add(text.hide, 0);
   tlHide.add(lineGenerator.stop);
+  tlHide.to('.overlay', 0.5, { autoAlpha: 1, onComplete }, '-=1.5');
 });
